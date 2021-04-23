c663c361e8c70068b747258a5510f188eb34ef9c
xiaofan <xiaofan@sensetime.com>
2021-04-23 11:12:47 +0800

    1

--- util/pdf_windows.go
+++ util/pdf_windows.go
@@ -36,27 +36,7 @@ func Pdf2Images(src []byte, dpi float64, pageLimit int) ([][]byte, error) {
 	}
 	return ret, nil
 }
-func Pdf2Images2(src []byte, dpi float64, pageLimit int) ([][]byte, error) {
-	if dpi <= 0 {
-		dpi = defaultDPI
-	}
-	doc, err := fitz.NewFromMemory(src)
-	if err != nil {
-		return nil, err
-	}
-	defer doc.Close()
-	if pageLimit > 0 && doc.NumPage() > pageLimit {
-		return nil, PageSizeErr
-	}
-	ret := make([][]byte, doc.NumPage())
-	for n := 0; n < doc.NumPage(); n++ {
-		ret[n], err = doc.ImagePNG1(n, dpi)
-		if err != nil {
-			return nil, err
-		}
-	}
-	return ret, nil
-}
+
 func Pdf2Images1(src []byte, dpi float64, pageLimit int) ([][]byte, error) {
 	if dpi <= 0 {
 		dpi = defaultDPI
